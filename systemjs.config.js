2368882e1b3ce490a7c9a3f2bb7229310f9484fa
Sergey Akopkokhyants <akserg@gmail.com>
2017-01-21 23:38:49 +0200

    Updated to Angular 2.4.4

--- systemjs.config.js
+++ systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      dist: 'dist',
+      dist: 'src',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
